4b7b95f6a301c1070e2659a7ff5ec812979c927c
Your Name <charles.paul@gmail.com>
2013-01-12 09:56:45 -0600

    redirect rather than render register

--- digitial_tip_jar/views.py
+++ digitial_tip_jar/views.py
@@ -100,7 +100,7 @@ def facebook_authorized(resp):
       session['fb_id'] = me.data['id']
       session['fb_username'] = me.data['username']
       session['fb_email'] = me.data['email']
-      return render_template('register.html')
+      return redirect(url_for('register'))
 
 
 @facebook.tokengetter
