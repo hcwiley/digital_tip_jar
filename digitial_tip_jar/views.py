c07c9af9061259056d420505bdbcff02a1b89f4c
Bryan Joseph <bryanjos@gmail.com>
2013-01-12 09:28:19 -0600

    New UI Updates. Added Error pages

--- digitial_tip_jar/views.py
+++ digitial_tip_jar/views.py
@@ -18,6 +18,14 @@ facebook = oauth.remote_app('facebook',
     request_token_params={'scope': 'email'}
 )
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def server_error(e):
+    return render_template('500.html'), 500
+
 
 @app.route('/')
 def index():
