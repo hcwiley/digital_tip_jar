548d9ca1d9e9b2c4cc61c92c21d010623cb662f5
Your Name <charles.paul@gmail.com>
2013-01-12 05:24:16 -0600

    reference config.FACE...

--- digitial_tip_jar/views.py
+++ digitial_tip_jar/views.py
@@ -12,8 +12,8 @@ facebook = oauth.remote_app('facebook',
     request_token_url=None,
     access_token_url='/oauth/access_token',
     authorize_url='https://www.facebook.com/dialog/oauth',
-    consumer_key=FACEBOOK_APP_ID,
-    consumer_secret=FACEBOOK_APP_SECRET,
+    consumer_key=config.FACEBOOK_APP_ID,
+    consumer_secret=config.FACEBOOK_APP_SECRET,
     request_token_params={'scope': 'email'}
 )
 
