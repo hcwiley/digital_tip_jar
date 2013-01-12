bee25e143e517c00bd1a7e14852a180a68fbf3f8
Bryan Joseph <bryanjos@gmail.com>
2013-01-11 21:35:38 -0600

    Example of template filling

--- digitial_tip_jar/views.py
+++ digitial_tip_jar/views.py
@@ -2,13 +2,15 @@ import config
 from flask import request, render_template
 from digitial_tip_jar import app
 from utils import slugify
+from user import *
 
 
 
 
 @app.route('/', methods=['POST','GET'])
 def index():
-    return render_template('index.html')
+    users = get_users()
+    return render_template('index.html', users=users)
 
 
 
