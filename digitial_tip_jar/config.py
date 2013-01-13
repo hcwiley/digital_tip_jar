7792055b902337368c202b4bc329e4d120520bc5
H. Cole Wiley <hcwiley@gmail.com>
2013-01-12 21:35:41 -0600

    added project root dir getter

--- digitial_tip_jar/config.py
+++ digitial_tip_jar/config.py
@@ -1,7 +1,8 @@
+import os
 SECRET_KEY = '1JZPwvT7HkNhTWxoACAdQYkYqwbwoWmLpSGgArMF'
 ERROR_LOG_PATH = '/root/error.log'
 STATIC_URL = '/static/'
-QR_PATH = '/root/digital_tip_jar/digitial_tip_jar'
+QR_PATH = os.path.abspath(os.path.dirname(__file__)) 
 MONGODB_HOST = 'localhost'
 MONGODB_PORT = 27017
 
